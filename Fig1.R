bc9b0d464c70e8dba41c7cb816fd43c435d9c317
Jimmy Nunnally <20599410+JimmyNunnally@users.noreply.github.com>
2020-10-27 15:53:35 -0400

    Create Fig1.R

---
+++ Fig1.R
@@ -0,0 +1,19 @@
+#load library
+library(dplyr)
+
+#Download and unzip  -Note: you will need to set up a working directory if you  havent already done so 
+file <- "dataset4.zip"
+site <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(site, file,method="curl")
+unzip(file) 
+
+
+A <- read.table('household_power_consumption.txt', header=T, sep=";", na.strings="?")
+
+## set time variable
+B <- A[A$Date %in% c("1/2/2007","2/2/2007"),]
+SetTime <-strptime(paste( B$Date,  B$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+B <- cbind(SetTime,  B)
+
+##  Plot 1
+hist( B$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
